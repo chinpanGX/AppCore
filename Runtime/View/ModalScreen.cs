0e0678287e7d3737c3219ba195c49f0d155e89f3
ちんぱん <52692512+chinpanGX@users.noreply.github.com>
2025-04-05 02:03:07 +0900

    ModalのSortingOrderの初期値を10000にする

--- Runtime/View/ModalScreen.cs
+++ Runtime/View/ModalScreen.cs
@@ -7,8 +7,8 @@ namespace AppCore.Runtime
     {
         [SerializeField] private Camera screenCamera;
         private readonly Stack<IView> stack = new(0);
-        private int sortingOrder;
-        
+        private int sortingOrder = 10000;
+
         public void Push(IView view)
         {
             if (!stack.Contains(view))
